2ce77ff1bdb3648cf2152697d71ad228bb303ac7
Sindhu S <sindhus@live.in>
2016-02-12 10:32:36 +0530

    Add some rudimentary templates and index view

--- myapp/views/index.py
+++ myapp/views/index.py
@@ -1,5 +1,17 @@
 from myapp import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    user = {'nick': 'eggpuff'}
+    posts = [
+        {
+            'author': {'nick': 'eggpuff'},
+            'body': 'Beautiful day out here!'
+        },
+        {
+            'author': {'nick': 'dimsum'},
+            'body': 'om Nom NOM NOm'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
