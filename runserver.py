b66c10405fede94fbf2c4968efcdaa8815f17d9e
Sindhu S <sindhus@live.in>
2016-02-15 14:31:20 +0530

    Fix app server

--- runserver.py
+++ runserver.py
@@ -1,6 +1,4 @@
-import config
 from myapp import app
 
-print app.url_map
 if __name__ == '__main__':
-    app.run(config.DevelopmentConfig.SERVER_NAME, port=8080)
+    app.run('0.0.0.0', port=5000)
